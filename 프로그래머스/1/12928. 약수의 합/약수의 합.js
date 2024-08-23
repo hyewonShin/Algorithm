781e0404728f2aa9adaeadeaf82a0878ff3dfe63
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-08-23 22:26:23 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    
+    // 약수 구하기
+    for(let i=1; i < n+1; i++){
+        if(n % i === 0){
+        answer += i
+        } 
+    }
+    return answer;
+}
